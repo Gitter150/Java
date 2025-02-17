b6ce29a7d2722d2c039866468135dd8b77285197
Gitter150 <chinmaypanghri@gmail.com>
2025-02-17 19:57:23 +0530

    UPDATED linkedList.java

--- linkedList.java
+++ linkedList.java
@@ -54,32 +54,50 @@ class List {
         curr.next = null;
         this.tail = curr;
     }
-    void display() {
+    int len() {
+        if(this.head == null) return 0;
+        int count = 0;
+        Node curr = this.head;
+        while(curr != null) {
+            count++;
+            curr = curr.next;
+        }
+        return count;
+    }
+    void display(boolean Debug) {
         if(this.head == null) {
             throw new NoSuchElementException("The list is empty! Cannot display it.");
         }
         Node curr;
         curr = this.head;
-        while(curr != null) {
-            System.out.print((curr==this.head? "head -> ":" -> ") + "("+curr.data+")" + (curr.next==null? " -> null":""));
+        if(Debug) while(curr != null) {
+            System.out.print((curr==this.head? "head -> ":" -> ") + curr.data + (curr.next==null? " -> null":""));
+            curr = curr.next;
+        }
+        else while(curr != null) {
+            System.out.print(curr == this.tail? (curr.data) + " ]":(curr == this.head? ("[ "+curr.data +", "):(curr.data + ", ")));
             curr = curr.next;
         }
         System.out.println();
     }
-}
-
-class linkedList {
-    public static void main(String[] args) {
-        List lst = new List();
-        lst.push_back(0);
-        lst.push_back(1);
-        lst.push_back(2);
-        lst.display();
-        lst.pop_back();
-        lst.display();
+    void display() {
+        this.display(false);
+    }
+    int peek_front() {
+        if(this.head == null) throw new NoSuchElementException("List is empty! Can't retrieve front element");
+        return this.head.data;
+    } 
+    int peek_back() {
+        if(this.tail == null) throw new NoSuchElementException("List is empty! Can't retrieve front element");
+        return this.tail.data;
     }
 }
 
-
-
-
+public class linkedList {
+    public static void main(String[] args) {
+        List lst = new List();
+        for(int i=1;i<=5;i++) lst.push_back(i);
+        lst.display();
+        System.out.println("The length of the list is now "+lst.len());
+    }
+}
